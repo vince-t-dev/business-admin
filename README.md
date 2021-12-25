3c956edcf84ae1fd803d77fd492bc03e8ea4dc30
Blackie <vince@Vince-T.local>
2021-12-25 16:01:55 +0900

    updated files

---
+++ README.md
@@ -0,0 +1,27 @@
+## Quick start
+
+### Using NPM
+
+1. Make sure you have [Node.js](https://nodejs.org/en/) installed. Make sure the installed Node version is >= 8.10 and of npm >= 5.6
+
+```
+npm install
+```
+
+2. Start the app in development mode by running the following command in terminal:
+
+```
+npm run start
+```
+
+3. Open http://localhost:3000 to view it in the browser. Any changes you make to the code will be automatically reflected in the browser.
+
+4. If you want to generate the production files, change the `homepage` value from the `package.json` to the domain name that the app will be hosted on, and then run the following command in the terminal:
+
+```
+npm run build
+```
+
+## Documentation
+
+Every component, plugin and getting started is thoroughly documented on our [online documentation](https://demo.themesberg.com/volt-react-dashboard/#/documentation/quick-start).
---
+++ src/components/Navbar.js
@@ -0,0 +1,119 @@
+
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faCog, faEnvelopeOpen, faSearch, faSignOutAlt, faUserShield } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import { Row, Col, Nav, Form, Image, Navbar, Dropdown, Container, ListGroup, InputGroup } from 'react-bootstrap';
+
+import NOTIFICATIONS_DATA from "../data/notifications";
+import Profile3 from "../assets/img/team/profile-picture-5.jpg";
+
+export default (props) => {
+  const [notifications, setNotifications] = useState(NOTIFICATIONS_DATA);
+  const areNotificationsRead = notifications.reduce((acc, notif) => acc && notif.read, true);
+
+  const markNotificationsAsRead = () => {
+    setTimeout(() => {
+      setNotifications(notifications.map(n => ({ ...n, read: true })));
+    }, 300);
+  };
+
+
+  const Notification = (props) => {
+    const { link, sender, image, time, message, read = false } = props;
+    const readClassName = read ? "" : "text-danger";
+
+    return ( 
+      <ListGroup.Item action href={link} className="border-bottom border-light">
+        <Row className="align-items-center">
+          <Col className="col-auto">
+            <Image src={image} className="user-avatar lg-avatar rounded-circle" />
+          </Col>
+          <Col className="ps-0 ms--2">
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h4 className="h6 mb-0 text-small">{sender}</h4>
+              </div>
+              <div className="text-end">
+                <small className={readClassName}>{time}</small>
+              </div>
+            </div>
+            <p className="font-small mt-1 mb-0">{message}</p>
+          </Col>
+        </Row>
+      </ListGroup.Item>
+    );
+  };
+
+  return (
+    <Navbar sticky="top" variant="light" className="navbar-top bg-white" expanded>
+      <Container fluid className="px-0">
+        <div className="d-flex justify-content-between w-100">
+          <div className="navbar-brand d-flex align-items-center">
+            <i className="xpri-xpr-2 text-eggplant me-3"></i>
+            <h4 className="text-eggplant">MY BUSINESS</h4>
+          </div>
+          <Nav className="align-items-center">
+            <Dropdown as={Nav.Item} onToggle={markNotificationsAsRead} >
+              <Dropdown.Toggle as={Nav.Link} className="text-dark icon-notifications p-0">
+                <span className="icon icon-sm">
+                  <FontAwesomeIcon icon={faBell} className="bell-shake" />
+                  {areNotificationsRead ? null : <span className="icon-badge rounded-circle unread-notifications" />}
+                </span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="dashboard-dropdown notifications-dropdown dropdown-menu-lg dropdown-menu-center mt-2 py-0">
+                <ListGroup className="list-group-flush">
+                  <Nav.Link href="#" className="text-center text-primary fw-bold border-bottom border-light py-3">
+                    Notifications
+                  </Nav.Link>
+
+                  {notifications.map(n => <Notification key={`notification-${n.id}`} {...n} />)}
+
+                  <Dropdown.Item className="text-center text-primary fw-bold py-3">
+                    View all
+                  </Dropdown.Item>
+                </ListGroup>
+              </Dropdown.Menu>
+            </Dropdown>
+            <span className="divider"></span>
+            <Dropdown as={Nav.Item}>
+              <Dropdown.Toggle as={Nav.Link} className="p-0 m-0">
+                <div className="media d-flex align-items-center">
+                  {Profile3 ?
+                  <Image src={Profile3} className="user-avatar md-avatar rounded-circle me-3" />
+                  :
+                  <i className="xpri xpri-user me-3"></i>
+                  } 
+                  <div className="media-body ms-2 text-dark align-items-center d-none d-lg-block">
+                    <span>Bonnie Green</span>
+                    <i className="xpri-arrow-right ms-2 text-cherry"></i>
+                  </div>
+                </div>
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="user-dropdown dropdown-menu-right mt-2">
+                <Dropdown.Item className="fw-bold">
+                  <FontAwesomeIcon icon={faUserCircle} className="me-2" /> My Profile
+                </Dropdown.Item>
+                <Dropdown.Item className="fw-bold">
+                  <FontAwesomeIcon icon={faCog} className="me-2" /> Settings
+                </Dropdown.Item>
+                <Dropdown.Item className="fw-bold">
+                  <FontAwesomeIcon icon={faEnvelopeOpen} className="me-2" /> Messages
+                </Dropdown.Item>
+                <Dropdown.Item className="fw-bold">
+                  <FontAwesomeIcon icon={faUserShield} className="me-2" /> Support
+                </Dropdown.Item>
+
+                <Dropdown.Divider />
+
+                <Dropdown.Item className="fw-bold">
+                  <FontAwesomeIcon icon={faSignOutAlt} className="text-danger me-2" /> Logout
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
