ef45d6e3544e237ff27174f8077166507125a0bb
Vince T <vince@Blackie.local>
2022-01-16 09:29:00 +0900

    debugging posthandler and xpr folder structure issues

--- xpr/server_js/ajax/ajax_handler.js
+++ xpr/server_js/ajax/ajax_handler.js
@@ -1,9 +1,10 @@
 // ajax handler route
 const xpr_objects = require("/xpr/request");
+const utilities = require("/xpr/utilities");
 
 exports.process = function(context, options) {
     var api = xpr_objects.XprApi;
     let request = xpr_objects.XprRequest();
  
-    return { context, options, xpr_objects };
+    return { context, options, request, utilities };
 }
