19df5530618c8d7a159eac58fc29742e7066ea19
Vince T <vince@Blackie.local>
2022-01-19 11:28:57 +0900

    token validation and ssjs work

--- xpr/server_js/library.js
+++ xpr/server_js/library.js
@@ -2,11 +2,11 @@ const xpr_objects = require("/xpr/request");
 const api = xpr_objects.XprApi;
 
 // check authentication
-exports.checkAuth = function checkAuth(token) {    
-    let check_auth = api({
+exports.checkAuth = function checkAuth(accessToken) {    
+    let token = api({
         uri: "/auth/tokens/",
         method: "GET",
-        params: { "Token__eq": token }
+        params: { "Token__eq": accessToken }
     });
     let expiry = (new Date(token[0].Expiry)).toISOString();
     let today = (new Date()).toISOString();
