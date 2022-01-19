7c68d637aab480b1baba9086361e112529d1e41e
Vince T <vince@Blackie.local>
2022-01-19 10:57:32 +0900

    token validation and ssjs work

--- xpr/server_js/library.js
+++ xpr/server_js/library.js
@@ -1,17 +1,11 @@
-function buildLibrary() {
-    return {
-        // check authentication
-        checkAuth: function(token) {
-            let token = api({
-                uri: "/auth/tokens/",
-                method: "GET",
-                params: { "Token__eq": token }
-            });
-            var expiry = (new Date(token[0].Expiry)).toISOString();
-            var today = (new Date()).toISOString();
-            return { auth_error: (Date.parse(expiry) <= Date.parse(today) || !token.length) }
-        }
-    }
+exports.checkAuth = function checkAuth(token) {
+    // check authentication
+    let token = api({
+        uri: "/auth/tokens/",
+        method: "GET",
+        params: { "Token__eq": token }
+    });
+    var expiry = (new Date(token[0].Expiry)).toISOString();
+    var today = (new Date()).toISOString();
+    return { auth_error: (Date.parse(expiry) <= Date.parse(today) || !token.length) }
 }
-
-module.exports = buildLibrary();
